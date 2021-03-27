00d8b1515c6b660127d9891e3bdf56beb86bcd22
rromang <80008461+rromang@users.noreply.github.com>
2021-03-27 16:44:20 -0700

    added comment for needed actions for script

---
+++ PyPoll/main.py
@@ -0,0 +1,7 @@
+#Task: Modernize voting counting
+
+#Script needs to calculate: 1) total number of votes cast, 
+# 2)complete list of candidates who received votes, 
+# 3)percentage of votes each candidate won, 
+# 4)total number of votes each candidate won 
+# 5)winner of the election based on popular vote
