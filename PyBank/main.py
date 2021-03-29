dcbe1312f3460882781887f54ecd68487dfe421d
rromang <80008461+rromang@users.noreply.github.com>
2021-03-28 22:16:14 -0700

    change in comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,10 +2,8 @@
 
 #Script needs to calculate: 1) total number of months in dataset, 
 # 2)net total amt of profit/losses over entire period, 
-# 3)changes in "Profit/Losses" over entire period, 
-# then find the average of those changes, 
-# 4)greatest increase in profits (date and amount) 
-# over entire period, 
+# 3)changes in "Profit/Losses" over entire period, then find the average of those changes, 
+# 4)greatest increase in profits (date and amount) over entire period, 
 # 5)greatest decrease in losses (date and amount) over entire period
 
 import os
