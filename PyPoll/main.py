e1541593ca539637f27487ce72230dc4fa08b548
rromang <80008461+rromang@users.noreply.github.com>
2021-03-29 18:41:01 -0700

    (WIP) adding code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,7 @@
 #Task: Modernize voting counting
 
-#Script needs to calculate: 1) total number of votes cast, 
+#Script needs to calculate: 
+# 1) total number of votes cast, 
 # 2)complete list of candidates who received votes, 
 # 3)percentage of votes each candidate won, 
 # 4)total number of votes each candidate won 
@@ -8,14 +9,9 @@
 
 import os
 import csv
+import pandas as pd
 
 poll_path = os.path.join('PyPoll','Resources','election_data.csv')
 
-
-
-with open(poll_path, 'r') as csvfile:
-
-    # Split the data on commas
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-    header = next(csvreader)
+df = pd.read_csv(poll_path)
+print(df)
