5798337b3ab6e4d6354e5005d3c56fc334da745a
rromang <80008461+rromang@users.noreply.github.com>
2021-04-01 00:04:42 -0700

    (WIP) calculated votes and percentages

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,6 +11,7 @@ import os
 import csv
 import pandas as pd
 
+
 poll_path = os.path.join('PyPoll','Resources','election_data.csv')
 
 poll_df = pd.read_csv(poll_path)
@@ -20,12 +21,19 @@ def summary_data():
    heading2 = "--------------------------------"
 
    total_votes = len(poll_df)  #calculates total votes by counting the length of the data frame 
-   #columns = poll_df.columns
-   #index_cand_df = poll_df.set_index("Candidate")
-   #unique_cand = poll_df["Candidate"].unique()
-   counts_df = poll_df.value_counts()
-   grouped_poll_df = pd.DataFrame(counts_df.groupby(["Candidate"]).sum())
-   sorted_df = grouped_poll_df.sort_values("Candidate", ascending=False)
+   
+   
+   votes = poll_df['Candidate'].value_counts()
+
+   perc_votes = ((poll_df['Candidate'].value_counts())/total_votes)*100
+   
+   
+   
+
+   #grouped_poll_df = pd.DataFrame(counts_df.groupby(["Candidate"]).sum())
+   
+   #grouped_poll_df.rename(columns={'Candidate': "Candidate", "Votes":"Votes"})
+   #sorted_df = grouped_poll_df.sort_values("Candidate", ascending=False)
 
    
    print(heading1)
@@ -33,7 +41,7 @@ def summary_data():
    print(f'Total Votes: {total_votes}')
    print(heading2)
    
-   print(sorted_df)
+   print(perc_votes
    
 
 summary_data()
