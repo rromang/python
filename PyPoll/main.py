84c760114217536075fa633f3c70c4b76033fe10
rromang <80008461+rromang@users.noreply.github.com>
2021-03-31 23:18:54 -0700

    (WIP) changes to summary function

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,5 +13,27 @@ import pandas as pd
 
 poll_path = os.path.join('PyPoll','Resources','election_data.csv')
 
-df = pd.read_csv(poll_path)
-print(df)
+poll_df = pd.read_csv(poll_path)
+
+def summary_data():
+   heading1 = "Election Results"
+   heading2 = "--------------------------------"
+
+   total_votes = len(poll_df)  #calculates total votes by counting the length of the data frame 
+   #columns = poll_df.columns
+   #index_cand_df = poll_df.set_index("Candidate")
+   #unique_cand = poll_df["Candidate"].unique()
+   counts_df = poll_df.value_counts()
+   grouped_poll_df = pd.DataFrame(counts_df.groupby(["Candidate"]).sum())
+   sorted_df = grouped_poll_df.sort_values("Candidate", ascending=False)
+
+   
+   print(heading1)
+   print(heading2)
+   print(f'Total Votes: {total_votes}')
+   print(heading2)
+   
+   print(sorted_df)
+   
+
+summary_data()
